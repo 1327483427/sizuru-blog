10779dbb837af0c3158f475df48a7650ae367724
sizuru <1327483427@qq.com>
2024-07-03 06:54:05 +0800

    20240703

--- src/components/BlogHeader.vue
+++ src/components/BlogHeader.vue
@@ -31,16 +31,28 @@
     </el-menu-item>
     <div class="flex-grow" />
     <el-input
-      style="width: 200px"
       v-model="searchText"
-      placeholder="请输入关键字"
-      class="search-input"
+      class="serach-input"
+      placeholder="搜索"
+      :prefix-icon="Search"
     />
+
+    <el-sub-menu index="2" popper-offset="-5">
+      <template #title>
+        <el-icon class="el-icon-setting">
+          <Calendar />
+        </el-icon>
+      </template>
+      <el-menu-item index="2-1">新浪微博</el-menu-item>
+      <el-menu-item index="2-2">github</el-menu-item>
+      <el-menu-item index="2-3">退出</el-menu-item>
+    </el-sub-menu>
   </el-menu>
 </template>
 
 <script lang="ts" setup>
 import { ref, computed } from "vue";
+import { Calendar, Search } from "@element-plus/icons-vue";
 const searchText = ref("");
 </script>
 
@@ -70,5 +82,38 @@ const searchText = ref("");
 
 .flex-grow {
   flex-grow: 1;
+}
+
+.serach-input {
+  width: 160px;
+  margin: 10px;
+  //圆角
+  border-radius: 20px;
+  //边框
+  border: 1px solid #ccc;
+  //字体大小
+  font-size: 18px;
+  //字体颜色
+  color: #333;
+  //输入框内文字居中
+  text-align: center;
+  //输入框内文字垂直居中
+  display: flex;
+  //字体加粗
+  font-weight: bold;
+
+  align-items: center;
+  //输入框内文字水平居中
+  justify-content: center;
+  //输入框内边距
+  padding: 0 10px;
+  //输入框内图标
+}
+
+:deep(.el-input__wrapper) {
+  box-shadow: 0 0 0 0px var(--el-input-border-color, var(--el-border-color))
+    inset;
+  cursor: default;
+  border: none !important;
 }
 </style>
